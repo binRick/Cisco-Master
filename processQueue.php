cc28cce9461ce101d06c5b6e64ca157487923f6c
binRick <rapidvps@gmail.com>
2015-09-10 19:23:48 -0400

    i

--- processQueue.php
+++ processQueue.php
@@ -2,24 +2,20 @@
 include_once './vendor/autoload.php';
 use Kue\Kue;
 if(!class_exists('Kue\Kue', true))
-	die('Kue class not found..  composer require kue/kue');
+    die('Kue class not found..  composer require kue/kue');
 
 
 
 $kue = Kue::createQueue(array('host' => 'beo.infinitumtech.net', 'port' => '8100'));
-print('connected to redis server..
-	');
+print('connected to redis server..');
 
 
 $kue->process('email', function($job){
-	print('email job received
-';
-	$data = $job->data;
-	echo 'sending email to ' . $data['to'] . '
-';
+    print('email job received');
+    $data = $job->data;
+    echo 'sending email to ' . $data['to'] . '';
 });
 
 
 $kue->process();
-print('waiting for jobs to be submitted
-	');
+print('waiting for jobs to be submitted');
