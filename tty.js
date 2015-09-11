9fd01b5a65e6d7a6ddfa7f3b0712fe6d91a963e4
binRick <rapidvps@gmail.com>
2015-09-10 21:25:33 -0400

    i

--- tty.js
+++ tty.js
@@ -3,13 +3,13 @@ var tty = require('tty.js');
 var port = 8002;
 var script = 'processQueue.php';
 var shell = 'php';
-var shellArgs = ['-f',script];
+var shellArgs = ['-f', script];
 
 var termApp = tty.createServer({
     shell: shell,
     shellArgs: shellArgs,
     static: './static',
-    cwd: __dirname, 
+    cwd: __dirname,
     port: port,
 });
 termApp.listen();
