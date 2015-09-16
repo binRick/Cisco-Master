13007e877a8ada2420fdc446a54c2f6702c7327c
Rick <rick@rick.local>
2015-09-16 14:14:25 -0600

    iiwq

--- phpServer.js
+++ phpServer.js
@@ -1,18 +1,26 @@
-var tty = require('tty.js');
+var tty = require('tty.js'),
+    child = require('child_process');
 var shell = 'php';
 var _ = require('underscore');
-var scripts = ['processQueue.php', 'createQueue.php'];
-var port = 8005;
-_.each(scripts, function(script) {
+var config = require('./config');
+var noide = require('noide'),
+    c = require('chalk');
 
+
+process.env['PORT']=config.phpserver.editorport;
+var phpEditorApp = require('noide');
+//phpEditorApp.listen(config.phpserver.editorport);
+console.log(c.green.bold('Editor server listening on port ') + c.red.bold(config.phpserver.editorport));
+
+_.each(config.phpserver.scripts, function(script) {
     var shellArgs = ['-f', script];
     var termApp = tty.createServer({
         shell: shell,
         shellArgs: shellArgs,
         static: './static',
         cwd: __dirname,
-        port: port,
+        port: config.phpserver.port,
     });
-    port++;
+    config.phpserver.port++;
     termApp.listen();
 });
